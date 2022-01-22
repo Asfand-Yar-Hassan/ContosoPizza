c38ca5cd3d906877a278083acd721d8d1b7d4853
Asfand Yar Hassan <asfandyarhassan341@gmail.com>
2022-01-21 21:56:14 -0500

    2nd Commit

--- Controllers/PizzaController.cs
+++ Controllers/PizzaController.cs
@@ -11,15 +11,46 @@ public class PizzaController : ControllerBase
 {
     public PizzaController()
     { }
+    //Returns a list of all pizza
     [HttpGet]
     public ActionResult<List<Pizza>> GetAll() => PizzaService.GetAll();
+    //Return pizza for a specific id
     [HttpGet("{id}")]
-    public ActionResult<Pizza>Get(int id)
+    public ActionResult<Pizza> Get(int id)
     {
         var pizza = PizzaService.Get(id);
         if (pizza == null)
             return NotFound();
 
         return pizza;
+    }
+    //Create and add a new pizza
+    [HttpPost]
+    public IActionResult Create(Pizza pizza)
+    {
+        PizzaService.Add(pizza);
+        return CreatedAtAction(nameof(Create), new { id = pizza.Id }, pizza);
+    }
+    //Delete a pizza by id
+    [HttpDelete("{id}")]
+    public IActionResult Delete (int id)
+    {
+        var pizza = PizzaService.Get(id);
+        if (pizza is null)
+            return NotFound();
+        PizzaService.Delete(id);
+        return NoContent();
+    }
+    //Update pizza by id
+    [HttpPut("{id}")]
+    public IActionResult Update(int id, Pizza pizza)
+    {
+        if (id != pizza.Id)
+            return BadRequest();
+        var existingPizza = PizzaService.Get(id);
+        if (existingPizza is null)
+            return NotFound();
+        PizzaService.Update(pizza);
+        return NoContent();
     }
 }
